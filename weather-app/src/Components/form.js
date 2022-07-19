51166b6978bcbd375d6414933afbc469e890fc5d
Anna Metsäpelto <anna.metsapelto@gmail.com>
2022-07-19 14:44:33 +0300

    Create the form

--- weather-app/src/Components/form.js
+++ weather-app/src/Components/form.js
@@ -1,7 +1,21 @@
+import { useState } from 'react';
+
 function Form() {
+    const [cityName, setCityname] = useState('');
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      setCityname('');
+    }
+
     return (
         <div>
             <h3>Enter the city below</h3>
+            <form onSubmit={event => handleSubmit(event)}>
+                <label for="city">City:</label>
+                <input type="text" id="city" onChange={e => setCityname(e.target.value)} value={cityName}></input>
+                <input type="submit" value="Search"></input>
+            </form>
+            <p>{cityName}</p>
         </div>
     )
 }
