ec04d96c522818a67931a3e341cdb8c2bf342675
Anna Metsäpelto <anna.metsapelto@gmail.com>
2022-07-31 13:05:12 +0300

    Work on the latest searches list

--- weather-app/src/Components/recents.js
+++ weather-app/src/Components/recents.js
@@ -1,10 +1,42 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function Recents( {key, city}) {
-    const [cityArray, setCityArray] = useState([]);
+    const [cityArray, setCityArray] = useState([{"city": '', "key": ''}]);
+
+useEffect(() => {
+    let newCity = [{"city": city, "key": key}];
+    console.log("Recents");
+    if (cityArray.length < 1) {
+        setCityArray({...cityArray, ...newCity})
+        console.log("Array is: " + cityArray)
+    }
+    if (doesNotContainCity(newCity)) {
+        if (cityArray.length < 10) {
+            setCityArray({...cityArray, ...newCity})
+        }
+        else {
+            let tempArray = cityArray;
+            tempArray.pop();
+            
+        }
+
+    }
+
+}, [cityArray], [city])
+
+    function doesNotContainCity(newCity) {
+        for (let i in cityArray) {
+            if (cityArray[i] === newCity) {
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
 
     return (
         <div>
+            <h4>Recent searches:</h4>
             <ul>
                 <li></li>
             </ul>
